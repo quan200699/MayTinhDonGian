457acaba8efeae7b8cdf0ce0b7fe2449db918d13
quan200699 <51079189+quan200699@users.noreply.github.com>
2019-05-30 08:39:08 +0700

    .

--- JS.js
+++ JS.js
@@ -1,43 +1,30 @@
 var number = "";
 var operator = "";
+var Array = []  ;
 function Number(Value) {
     number = document.getElementById("result").value;
     document.getElementById("result").value = number+""+ Value;
     number = Value;
+    Array.push(number);
 }
 function Operator(Value) {
     operator = document.getElementById("result").value;
     document.getElementById("result").value = operator +""+Value;
     operator = Value;
+    Array.push(operator);
 }
 function Result() {
-    var result = 0;
-        switch (operator) {
-            case "+":
-            {
-                result = parseInt(document.getElementById("result").value)+parseInt(number);
-                break;
-            }
-            case "-":
-            {
-                result = parseInt(document.getElementById("result").value)-parseInt(number);
-                break;
-            }
-            case "*":
-            {
-                result = parseInt(document.getElementById("result").value)*parseInt(number);
-                break;
-            }
-            case "/":
-            {
-                result =parseInt(document.getElementById("result").value)/ parseInt(number);
-                break;
-            }
-        }
+     var result = 0;
+    for(var i = 0;i<Array.length;i++)
+    {
+        result+=Array[i];
+    }
+    result = eval(result);
     document.getElementById("result").value=result;
 }
 function Clear() {
     number = "";
     operator = "";
+    Array = [];
     document.getElementById("result").value="";
 }
